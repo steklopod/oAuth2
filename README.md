19ec2006f1850424f60bc350b9c3099f4f83913e
Dmitry Koltovich <steklopod@gmail.com>
2020-05-23 17:13:04 +0300

    stash

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-![Backend CI](https://github.com/steklopod/oAuth2/workflows/oAuth2%20CI/badge.svg) 
+# oAuth2 server ![Backend CI](https://github.com/steklopod/oAuth2/workflows/oAuth2%20CI/badge.svg) 
+
+### Links:
+* [localhost:585/oauth2/authorization/google](http://localhost:585/oauth2/authorization/google)
+
+> [Spring docs](https://docs.spring.io/spring-security/site/docs/current/reference/html5/#oauth2login)
--- src/main/kotlin/online/colaba/oauth2/OAuth2Application.kt
+++ src/main/kotlin/online/colaba/oauth2/OAuth2Application.kt
@@ -1,10 +1,13 @@
 package online.colaba.oauth2
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -17,36 +20,50 @@ fun main(args: Array<String>) {
 }
 
 
-@RestController
+@RestController("/")
 class Controller {
-    @GetMapping("/")
-    fun helloWorld(): String {
-        return "MAIN: you don't need to be logged in"
+    companion object {
+        private val log = LoggerFactory.getLogger(this::class.java)
     }
 
-    @GetMapping("/not-restricted")
-    fun notRestricted(): String {
-        return "not-restricted: you don't need to be logged in"
+    @GetMapping
+    fun index(
+//        @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+//        @AuthenticationPrincipal OAuth2User oauth2User
+    ): String {
+        log.warn("ROOT ")
+        return "MAIN (root) controller path '/': \uD83D\uDC4D \uD83D\uDC4D \uD83D\uDC4D OK"
     }
 
-    @GetMapping("/restricted")
-    fun restricted(): String {
-        return "if you see this you are logged in"
+
+    @GetMapping("/auth")
+    fun index2(
+    ): String {
+        log.warn("/auth: SECOND")
+        return "2 controller path '/': \uD83D\uDC4D  OK"
     }
+
+
+    @GetMapping("/success")
+    fun index3(
+    ): String {
+        log.warn("/success: 3")
+        return "3 controller path '/': \uD83D\uDC4D  OK"
+    }
+
+
 }
 
 
-@Configuration
-class SecurityConfig : WebSecurityConfigurerAdapter() {
+@EnableWebSecurity
+class OAuth2LoginSecurityConfig : WebSecurityConfigurerAdapter() {
     @Throws(Exception::class)
     public override fun configure(http: HttpSecurity) {
         http
-            .antMatcher("/**").authorizeRequests()
-            .antMatchers(
-                "/not-restricted"
-            ).permitAll()
-            .anyRequest().authenticated()
-            .and()
             .oauth2Login()
+            .defaultSuccessUrl("/success")
+//            .redirectionEndpoint()
+//            .baseUri("/auth")
+
     }
 }
